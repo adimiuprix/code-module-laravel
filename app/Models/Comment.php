0af3fdc974fdf536242eb04214095ac8dcb9bc96
adimiuprix <miuprix@gmail.com>
2024-02-22 19:59:04 +0700

    test

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
     use HasFactory;
 
     // Di bagian ini kita sambungin Model post
-    public function post(){
+    public function post() : BelongsTo
+    {
         return $this->belongsTo(Post::class);   // gunakan method belongsTo dengan parameter nya model Post
     }
 }
